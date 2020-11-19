2259f75eb2e030a1e32786916625fb5d80506666
KASTURI_K <kasturikulkarni711@gmail.com>
2020-11-19 22:19:41 +0530

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <consumer-analytics></consumer-analytics>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import consumerAnalytics from './components/consumerAnalytics.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    consumerAnalytics
   }
 }
 </script>
